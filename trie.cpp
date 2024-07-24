4fa631a81449a588b7015337b0e68296de89a3d2
SamantaSoumya <soumyasamanta354@gmail.com>
2024-07-24 20:50:13 +0530

    trie

--- trie.cpp
+++ trie.cpp
@@ -1,1 +1,64 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    Node* links[26];
+    bool flag=false;
+
+    bool containsKey(char c){
+        return links[c-'a']!=NULL;
+    }
+
+    void put(char c,Node* temp){
+        links[c-'a']=temp;
+    }
+    Node* get(char c){
+        return links[c-'a'];
+    }
+    void setEnd(){
+        flag=true;
+    }
+};
+class Trie{
+    private:
+        Node * root;
+    public:
+        Trie(){
+            root = new Node();
+        }
+        void insert(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+        }
+        bool search(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containsKey(word[i])) {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->flag;
+        }
+        bool startsWith(string prefix) {
+        Node* node = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if (!node->containsKey(prefix[i])) {
+                return false;
+            }
+            node = node->get(prefix[i]);
+        }
+        return true;
+    }
+};
+int main(){
+    Trie trie;
+    trie.insert("soumya");
+    return 0;
+}
