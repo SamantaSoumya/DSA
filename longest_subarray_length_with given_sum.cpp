364c18bd00cdf029463131c0f88d5e2f68cd9f4e
SamantaSoumya <soumyasamanta354@gmail.com>
2024-11-15 14:02:35 +0530

    for positive element

---
+++ longest_subarray_length_with given_sum.cpp
@@ -0,0 +1,30 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int lenOfLongestSubarr(vector<int> &arr, int k)
+{
+    int maxLen = 0;
+    int n = arr.size();
+    unordered_map<long long, int> mp;
+    long long sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        mp[sum] = i;
+        if (sum == k)
+        {
+            maxLen = max(maxLen, i + 1);
+        }
+        long long rem = sum - k;
+        if (mp.find(rem) != mp.end())
+        {
+            maxLen = max(maxLen, i - mp[rem]);
+        }
+    }
+    return maxLen;
+}
+int main(){
+    vector<int>arr;
+    int k;
+    int ans = lenOfLongestSubarr(arr,k);
+}
